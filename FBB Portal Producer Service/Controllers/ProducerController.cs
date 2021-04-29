337bc79eba9aba355e853487881531e8235d612c
tarathep <bokie.tarathep@gmail.com>
2021-04-29 17:33:58 +0700

    init proj

---
+++ FBB Portal Producer Service/Controllers/ProducerController.cs
@@ -0,0 +1,44 @@
+﻿using Confluent.Kafka;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FBB_Portal_Service.Controllers
+{
+
+    [ApiController]
+    [Route("api/")]
+    public class ProducerController : ControllerBase
+    {
+        private ProducerConfig _config;
+        public ProducerController(ProducerConfig config)
+        {
+            this._config = config;
+        }
+
+
+        [HttpGet("send")]
+        public async Task<ActionResult> GetAsync()
+        {
+            //string serializedEmployee = JsonConvert.SerializeObject(employee);
+            string serializedEmployee = "{\"A\":\"B\"}";
+
+            string topic = "HOME_CHECKIN_CREATED";
+            using (var producer = new ProducerBuilder<Null, string>(_config).Build())
+            {
+                await producer.ProduceAsync(topic, new Message<Null, string> { Value = serializedEmployee });
+                producer.Flush(TimeSpan.FromSeconds(10));
+                return Ok("Okkk");
+            }
+        }
+
+        public class Employee
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
